5a43dba11a4cd60a63835c40550f2ab5c93c5708
isamw <isamw@ntnu.nu>
2025-09-11 12:05:51 +0200

    made Flashcard.java and textfields in FlashcardController.java. edited name from javafx.template to gr2516 in json and pom.xml files

--- gr2516/fxui/src/main/java/ui/FlashcardController.java
+++ gr2516/fxui/src/main/java/ui/FlashcardController.java
@@ -1,3 +1,20 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class FlashcardController {
-  
+  @FXML private TextField questionField;
+  @FXML private TextField answerField;
+  @FXML private ListView listView;
+
+  @FXML public void initialize(){
+    updateUI();
+  }
+
+  public void updateUI(){
+    //set the flashcard in to the list
+  }
+
 }
