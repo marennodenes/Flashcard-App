bd2b394545be6c0fb3a2514fa17228308cfa6438
ailintinglum <ailinat@stud.ntnu.no>
2025-09-27 19:18:59 +0200

    feat(core): user class for saving username and password for each user
    co-authored by: sofietw
    #88

---
+++ flashcards/core/src/main/java/app/User.java
@@ -0,0 +1,22 @@
+package app;
+
+/**
+ * Represents a user with a username and password.
+ */
+public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
